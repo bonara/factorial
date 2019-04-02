0a4ee42cf0781524b981aa9a26163bbf5fec58bc
Nara <naraa_520@yahoo.com>
2019-04-01 20:57:17 -0700

    Added factorial function with all tests passing.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,16 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the number of digits
+# Space complexity: O(1)
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError.new("Nil object is not an integer") if number == nil
+  if number == 0 || number == 1
+    return 1
+  elsif number > 0
+    value = 1 
+    while number > 0
+      value *= number
+      number -= 1
+    end
+    return value
+  end
 end
